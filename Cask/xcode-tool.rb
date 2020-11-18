e67f982c3c0eee95f55430f5878f8ae0a84c49a9
TBXark <tbxark@outlook.com>
2020-11-18 16:34:55 +0800

    Update xcode-tool.rb

--- Cask/xcode-tool.rb
+++ Cask/xcode-tool.rb
@@ -2,8 +2,8 @@ cask 'xcode-tool' do
   version '1.0.1'
   sha256 "1ccd1cdf5b9aac64ff1be01c594c6bbda519eddf045b09b43ac53de6802134eb"
   url "https://github.com/TBXark/xcode-tool/releases/download/#{version}/xct-v#{version}.zip"
-  name 'xct'
-  homepage 'https://github.com/peripheryapp/periphery'
+  name 'xcode-tool'
+  homepage 'https://github.com/TBXark/xcode-tool'
   binary 'xct'
   depends_on macos: '>= :catalina'
 
