acbb060277737ec212d73efd712eb6871771a852
TBXark <tbxark@outlook.com>
2020-11-18 15:45:40 +0800

    Update xcode-tool.rb

--- Formula/xcode-tool.rb
+++ Formula/xcode-tool.rb
@@ -1,19 +1,13 @@
 class XcodeTool < Formula
   desc "A collection of useful tools for an xcode project"
-  homepage ""
+  homepage "https://github.com/TBXark/xcode-tool"
   url "https://github.com/TBXark/xcode-tool/releases/download/1.0.1/xct-v1.0.1.zip"
   sha256 "1ccd1cdf5b9aac64ff1be01c594c6bbda519eddf045b09b43ac53de6802134eb"
   license "MIT"
-
-
-  def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-  end
+  binary 'xct'
+  depends_on macos: '>= :catalina'
 
   test do
-    system "false"
+    system bin/"xct", "json", "1"
   end
 end
