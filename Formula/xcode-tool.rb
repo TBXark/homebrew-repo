7aaa4491dc67bf4193ef39207b3c9c7dae0c2328
TBXark <tbxark@outlook.com>
2020-11-18 18:12:49 +0800

    Update xcode-tool.rb

--- Formula/xcode-tool.rb
+++ Formula/xcode-tool.rb
@@ -1,5 +1,4 @@
 class XcodeTool < Formula
-  desc "A collection of useful tools for an xcode project"
   homepage "https://github.com/TBXark/xcode-tool"
   url "https://github.com/TBXark/xcode-tool/releases/download/1.0.1/xct-v1.0.1.zip"
   sha256 "fe9ceb8989bfe85f0545e496d968d3ebd18daaca8b491d6e44fec8e51e2cfe63"
