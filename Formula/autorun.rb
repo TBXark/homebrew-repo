b3910baae6d862950d62c00c672556ad9c66237b
TBXark <tbxark@outlook.com>
2021-01-26 18:01:18 +0800

    Update autorun.rb

--- Formula/autorun.rb
+++ Formula/autorun.rb
@@ -1,4 +1,4 @@
-class AutoRun < Formula
+class Autorun < Formula
   homepage "https://github.com/TBXark/autorun"
   url "https://github.com/TBXark/autorun/releases/download/0.0.1/autorun_0.0.1_darwin_amd64.tar.gz"
   sha256 "ec5cf4bf89cac22bcc17b7f24531bc2518379df79f305cdf605e1005f0e979d6"
