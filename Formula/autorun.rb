6a038816056b4d75a57e99e84816098f321f977b
TBXark <tbxark@outlook.com>
2021-12-29 16:07:22 +0800

    Update autorun.rb

--- Formula/autorun.rb
+++ Formula/autorun.rb
@@ -1,14 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Autorun < Formula
-  homepage "https://github.com/TBXark/autorun"
-  url "https://github.com/TBXark/autorun/releases/download/0.0.1/autorun_0.0.1_darwin_amd64.tar.gz"
-  sha256 "ec5cf4bf89cac22bcc17b7f24531bc2518379df79f305cdf605e1005f0e979d6"
+  desc "autorun is a tool that listens for file changes and automatically executes commands."
+  homepage ""
+  url "https://github.com/TBXark/autorun/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "23016f4835ef7d4e85817e5122119a39aea1ce4bc83c4fb61b996301dc44b2a2"
   license "MIT"
 
+  depends_on "go" => :build
+
   def install
-    bin.install "autorun"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
-    system "#{bin}/autorun -v"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test autorun`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
