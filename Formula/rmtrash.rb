dec688ac609024f9e676accd6c10fc0bf524d130
tbxark <tbxark@outlook.com>
2024-11-26 17:16:44 +0800

    chore: 更新rmtrash

--- Formula/rmtrash.rb
+++ Formula/rmtrash.rb
@@ -4,15 +4,15 @@
 class Rmtrash < Formula
   desc "rmtrash is a small utility that will move the file to OS X's Trash rather than obliterating the file (as rm does)."
   homepage "https://github.com/TBXark/rmtrash"
-  url "https://github.com/TBXark/rmtrash/archive/refs/tags/0.3.3.tar.gz"
-  sha256 "3acc58b297d60c084c3171809db322f13bc121f1bcf17c1feb8772b4db4a2f3d"
+  url "https://github.com/TBXark/rmtrash/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "4e8baaa84f0545ddc7c99cddbd5046beb87afc34719ea53a60e915aaeebdad36"
   license "MIT"
 
   # depends_on "make" => :build
 
   def install
-    system "make"
-    bin.install "rmtrash"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/rmtrash"
   end
 
   test do
