65599a3dcb66f70b34908d3e25c44df6e536a453
tbxark <tbxark@outlook.com>
2024-11-27 17:20:23 +0800

    chore: Update rmtrash SHA256 checksum for version 0.6.2

--- Formula/rmtrash.rb
+++ Formula/rmtrash.rb
@@ -5,11 +5,11 @@ class Rmtrash < Formula
   desc "rmtrash is a small utility that will move the file to macOS's Trash rather than obliterating the file (as rm does)."
   homepage "https://github.com/TBXark/rmtrash"
   url "https://github.com/TBXark/rmtrash/releases/download/0.6.2/rmtrash_0.6.2_universal.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "55253a283dba523d151515f31a4d87ea584c49c2a062629da68c29453632113f"
   license "MIT"
 
   depends_on :macos
-  
+
   def install
     bin.install "rmtrash"
   end
