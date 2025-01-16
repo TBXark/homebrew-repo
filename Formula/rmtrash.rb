84a01338ec9460d1feefca188b30bf16c806cdae
tbxark <tbxark@outlook.com>
2025-01-16 14:00:34 +0800

    chore: 清理 rmtrash 和 xcode-tool 的注释，简化代码

--- Formula/rmtrash.rb
+++ Formula/rmtrash.rb
@@ -1,30 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Rmtrash < Formula
-  desc "rmtrash is a small utility that will move the file to macOS's Trash rather than obliterating the file (as rm does)."
+  desc "Move files and directories to the trash"
   homepage "https://github.com/TBXark/rmtrash"
   url "https://github.com/TBXark/rmtrash/releases/download/0.6.5/rmtrash_0.6.5_universal.tar.gz"
   sha256 "5b3193675557d8e4eb321a80533bd90b27becfaf268711f3278a78c0ef5ac7fc"
   license "MIT"
-
   depends_on :macos
-
   def install
     bin.install "rmtrash"
     man1.install "rmtrash.1"
   end
-
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test rmtrash`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "rmtrash", "--version"
   end
 end
--- Formula/xcode-tool.rb
+++ Formula/xcode-tool.rb
@@ -1,31 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class XcodeTool < Formula
   desc "A collection of useful tools for an xcode project"
   homepage "https://github.com/TBXark/xcode-tool"
   url "https://github.com/TBXark/xcode-tool/archive/refs/tags/1.0.2.tar.gz"
   sha256 "86fa8a0210d9707f06a7e7891cd8bf6521b83cf4d2781759e6d48ecad5553dca"
   license "MIT"
-
   depends_on xcode: "10.2"
   depends_on :macos
-  
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/xct"
   end
-
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xcode-tool`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "xct", "--help"
   end
 end
